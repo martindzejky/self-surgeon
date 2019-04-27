2a15365088bd56917b605d0b6aedb67b286c1534
Martin Jakubik <martin@jakubik.info>
2019-04-27 15:47:50 +0200

    highlight body parts

--- Assets/Scripts/LargeHuman/BodyPart.cs
+++ Assets/Scripts/LargeHuman/BodyPart.cs
@@ -3,7 +3,23 @@
 public class BodyPart : MonoBehaviour {
     public string partName;
 
+    private BoxCollider2D boxCollider;
+
     public void OnEnable() {
         this.partName = this.gameObject.name;
+
+        var collider = this.GetComponent<BoxCollider2D>();
+        if (collider) {
+            this.boxCollider = collider;
+            Debug.Log("Collider found for " + this.partName);
+        }
+    }
+
+    public void OnMouseEnter() {
+        PartHighlighter.main.mouseOverParts.Push(this);
+    }
+
+    public void OnMouseExit() {
+        PartHighlighter.main.mouseOverParts.Pop();
     }
 }
---
+++ Assets/Scripts/LargeHuman/PartHighlighter.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PartHighlighter : MonoBehaviour {
+    public Stack<BodyPart> mouseOverParts = new Stack<BodyPart>();
+
+    public GameObject uiTextObject;
+
+    [HideInInspector]
+    public BodyPart currentOverPart;
+    [HideInInspector]
+    public BodyPart previousOverPart;
+
+    private Camera mainCamera;
+
+    public static PartHighlighter main {
+        get;
+        private set;
+    }
+
+    public void OnEnable() {
+        PartHighlighter.main = this;
+        this.mainCamera = Camera.main;
+    }
+
+    public void Update() {
+        if (this.mouseOverParts.Count > 0) {
+            this.currentOverPart = this.mouseOverParts.Peek();
+
+            if (!this.previousOverPart || this.previousOverPart != this.currentOverPart) {
+                Debug.Log("New body part under mouse " + this.currentOverPart.partName);
+                this.previousOverPart = this.currentOverPart;
+            }
+        } else {
+            this.currentOverPart = null;
+            this.previousOverPart = null;
+        }
+
+        if (this.currentOverPart) {
+            this.uiTextObject.SetActive(true);
+
+            var textComponent = this.uiTextObject.GetComponent<Text>();
+            textComponent.text = this.currentOverPart.partName;
+
+            this.HandleClicking();
+        } else {
+            this.uiTextObject.SetActive(false);
+        }
+    }
+
+    private void HandleClicking() {
+    }
+}
