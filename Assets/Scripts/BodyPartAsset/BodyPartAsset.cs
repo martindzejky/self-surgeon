961e0e98f70f4f592d40ed858c18611b08e7e3fb
Martin Jakubik <martin@jakubik.info>
2019-05-09 16:44:53 +0200

    add scriptable object for body parts

---
+++ Assets/Scripts/BodyPartAsset/BodyPartAsset.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Surgeon/Body part")]
+public class BodyPartAsset : ScriptableObject {
+    public BodyPartAssetType bodyPartType;
+    public string bodyPartName;
+}
