1723ba83c121b76822404db7af5b173907a78e44
Martin Jakubik <martin@jakubik.info>
2019-05-09 18:03:29 +0200

    add basic idle and walk animations

--- Assets/Objects/LargeHuman/Scripts/LargeHumanMovement.cs
+++ Assets/Objects/LargeHuman/Scripts/LargeHumanMovement.cs
@@ -3,10 +3,17 @@
 public class LargeHumanMovement : MonoBehaviour {
     public float movementSpeed;
 
+    private Animator animator;
+
+    private void Awake() {
+        this.animator = this.GetComponent<Animator>();
+    }
+
     private void Update() {
         var currentInput = Input.GetAxis("Horizontal");
+        var absCurrentInput = Mathf.Abs(currentInput);
 
-        if (Mathf.Abs(currentInput) > .1f) {
+        if (absCurrentInput > .1f) {
             var currentPosition = this.transform.position;
             currentPosition.x += this.movementSpeed * Time.deltaTime * currentInput;
             this.transform.position = currentPosition;
@@ -14,6 +21,10 @@ public class LargeHumanMovement : MonoBehaviour {
             var currentScale = this.transform.localScale;
             currentScale.x = -Mathf.Sign(currentInput);
             this.transform.localScale = currentScale;
+        }
+
+        if (this.animator) {
+            this.animator.SetFloat("movementSpeed", absCurrentInput);
         }
     }
 }
