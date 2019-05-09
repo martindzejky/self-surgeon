f402322c00859c7f51dac486c0279667c58c726e
Martin Jakubik <martin@jakubik.info>
2019-05-09 17:26:59 +0200

    setup camera for house scene

--- Assets/Objects/LargeHuman/Scripts/LargeHumanMovement.cs
+++ Assets/Objects/LargeHuman/Scripts/LargeHumanMovement.cs
@@ -1,6 +1,19 @@
 ﻿using UnityEngine;
 
 public class LargeHumanMovement : MonoBehaviour {
-    private void Awake() {
+    public float movementSpeed;
+
+    private void Update() {
+        var currentInput = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(currentInput) > .1f) {
+            var currentPosition = this.transform.position;
+            currentPosition.x += this.movementSpeed * Time.deltaTime * currentInput;
+            this.transform.position = currentPosition;
+
+            var currentScale = this.transform.localScale;
+            currentScale.x = -Mathf.Sign(currentInput);
+            this.transform.localScale = currentScale;
+        }
     }
 }
