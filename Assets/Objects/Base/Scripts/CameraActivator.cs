51c4fb97381f998658ce6b2565682439961f91e6
Martin Jakubik <martin@jakubik.info>
2019-06-08 08:45:58 +0200

    WIP

---
+++ Assets/Objects/Base/Scripts/CameraActivator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(Collider2D)), RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CameraActivator : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D other) {
+        GetComponent<CinemachineVirtualCamera>().enabled = true;
+        Debug.Log("Entered " + gameObject.name);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        GetComponent<CinemachineVirtualCamera>().enabled = false;
+        Debug.Log("Left " + gameObject.name);
+    }
+}
